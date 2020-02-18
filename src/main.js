1970083c9ec31abff09d4bec5b2d0e59224bf8f5
张鑫 <zxin088@gmail.com>
2020-02-18 13:27:35 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,25 @@ import router from './routers'
 import store from './store'
 import utils from './utils'
 import * as filters from './filters' // global filters
-import { Toast, Field, NavBar, Button, Divider, DropdownMenu, DropdownItem } from 'vant'
+
+import {
+  Toast,
+  Field,
+  NavBar,
+  Button,
+  Divider,
+  DropdownMenu,
+  DropdownItem,
+  Form,
+  Swipe,
+  SwipeItem,
+  Lazyload,
+  NoticeBar,
+  Tabbar,
+  TabbarItem,
+  Tab,
+  Tabs
+} from 'vant'
 Vue.use(Toast)
 Vue.use(Field)
 Vue.use(NavBar)
@@ -13,8 +31,19 @@ Vue.use(Button)
 Vue.use(Divider)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
+Vue.use(Form)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Lazyload)
+Vue.use(NoticeBar)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(Tab)
+Vue.use(Tabs)
+
 import VueWechatTitle from 'vue-wechat-title'
 Vue.use(VueWechatTitle)
+
 if (process.env.NODE_ENV === 'development' && process.env.VUE_APP_DEBUG === 'true') {
   import('eruda').then(module => {
     var el = document.createElement('div')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,39 @@
     <van-nav-bar
       title="Home"
     />
-    <h3 @click="$router.push('/demo')">跳转到Demo页</h3>
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image">
+      </van-swipe-item>
+    </van-swipe>
+    <van-notice-bar text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。" left-icon="volume-o" />
+    <van-tabs v-model="active">
+      <van-tab title="标签 1"><h3 @click="$router.push('/demo')">跳转到Demo页</h3></van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      active: 0,
+      images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg'
+      ]
+    }
+  },
   created() {
     this.$toast.loading({
       duration: 0,
@@ -25,3 +51,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+img{
+  height: 150px;
+}
+</style>
