c3e29696e378a091aa4ca4fa3e869c2a4cc151c1
张鑫 <zxin088@gmail.com>
2020-02-18 12:55:32 +0800

    处理无响应情况

--- src/utils/request.js
+++ src/utils/request.js
@@ -78,61 +78,66 @@ service.interceptors.response.use(
     return res
   },
   error => {
-    if (error && error.config && error.config.showLoading) {
-      Vue.prototype.$toast.clear()
-    }
-    if (error && error.response) {
-      switch (error.response.status) {
-        case 400:
-          error.message = '错误请求'
-          break
-        case 401:
-          error.message = '未授权，请重新登录'
-          break
-        case 403:
-          error.message = '拒绝访问'
-          break
-        case 404:
-          error.message = '请求错误,未找到该资源'
-          break
-        case 405:
-          error.message = '请求方法未允许'
-          break
-        case 408:
-          error.message = '请求超时'
-          break
-        case 500:
-          error.message = '服务器端出错'
-          break
-        case 501:
-          error.message = '网络未实现'
-          break
-        case 502:
-          error.message = '网络错误'
-          break
-        case 503:
-          error.message = '服务不可用'
-          break
-        case 504:
-          error.message = '网络超时'
-          break
-        case 505:
-          error.message = 'http版本不支持该请求'
-          break
-        default:
-          error.message = `连接错误${error.response.status}`
+    console.log(error.message)
+    if (error) {
+      if (error.config && error.config.showLoading) {
+        Vue.prototype.$toast.clear()
       }
-      const errData = {
-        code: error.response.status,
-        message: error.message
-      }
-      console.log('统一错误处理: ', errData)
-      if (errData.code >= 400 && errData.code < 500) {
-        Dialog({ title: '提示', message: '业务执行错误: ' + errData.message || 'Error' })
-      } else if (errData.code >= 500) {
-        Dialog({ title: '提示', message: '系统运行异常: ' + errData.message || 'Error' })
-      } else {
-        Dialog({ title: '提示', message: '其他异常: ' + errData.message || 'Error' })
+      if (error.response) {
+        switch (error.response.status) {
+          case 400:
+            error.message = '错误请求'
+            break
+          case 401:
+            error.message = '未授权，请重新登录'
+            break
+          case 403:
+            error.message = '拒绝访问'
+            break
+          case 404:
+            error.message = '请求错误,未找到该资源'
+            break
+          case 405:
+            error.message = '请求方法未允许'
+            break
+          case 408:
+            error.message = '请求超时'
+            break
+          case 500:
+            error.message = '服务器端出错'
+            break
+          case 501:
+            error.message = '网络未实现'
+            break
+          case 502:
+            error.message = '网络错误'
+            break
+          case 503:
+            error.message = '服务不可用'
+            break
+          case 504:
+            error.message = '网络超时'
+            break
+          case 505:
+            error.message = 'http版本不支持该请求'
+            break
+          default:
+            error.message = `连接错误${error.response.status}`
+        }
+        const errData = {
+          code: error.response.status,
+          message: error.message
+        }
+        console.log('统一错误处理: ', errData)
+        if (errData.code >= 400 && errData.code < 500) {
+          Dialog({ title: '提示', message: '业务执行错误: ' + errData.message || 'Error' })
+        } else if (errData.code >= 500) {
+          Dialog({ title: '提示', message: '系统运行异常: ' + errData.message || 'Error' })
+        } else {
+          Dialog({ title: '提示', message: '其他异常: ' + errData.message || 'Error' })
+        }
+      } else if (error.request) {
+        Vue.prototype.$toast('网络出错，请稍后重试')
       }
     }
     return Promise.reject(error)
